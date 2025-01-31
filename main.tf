214302e41eeca2f528d7d4ecc760a315026d937a
Ahmed Zein <ahuseein@banquemisr.com>
2025-01-31 22:55:57 +0200

    add teraform scripts

--- main.tf
+++ main.tf
@@ -18,6 +18,7 @@ module "eks" {
   cluster_name    = var.cluster_name
   vpc_id          = module.vpc.vpc_id
   private_subnets = module.vpc.private_subnet_ids
+  subnet_ids      = module.vpc.private_subnet_ids
   environment     = var.environment
 }
 
