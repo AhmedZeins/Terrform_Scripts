1bfcd3148452d39aed1179631675400d3ec699b3
Ahmed Zein <ahuseein@banquemisr.com>
2025-02-01 13:50:05 +0200

    add teraform scripts

--- main.tf
+++ main.tf
@@ -31,15 +31,3 @@ data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
   depends_on = [module.eks]
 }
-
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-module "k8s_resources" {
-  source = "./modules/k8s_resources"
-  cluster_name = var.cluster_name
-  depends_on = [module.eks]
-}
